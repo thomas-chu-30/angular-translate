68f5bab9009cbc5a2d346ca5c7ee2e95e3672a9f
thomas <thomas.chu@techdesign.com>
2023-11-03 14:42:56 +0800

    feat: update translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-angular-app-translate';
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'nl']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use('en');
+  }
+
+  translation$ = this.translate.onLangChange.pipe(
+    switchMap(({ lang }) => this.translate.getTranslation(lang))
+  );
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHandler,
+} from '@angular/common/http';
+import {
+  IModuleTranslationOptions,
+  ModuleTranslateLoader,
+} from '@larscom/ngx-translate-module-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+export function ModuleHttpLoaderFactory(http: HttpClient) {
+  const baseTranslateUrl = './assets/i18n';
+
+  const options: IModuleTranslationOptions = {
+    translateError: (error, path) => {
+      console.log('Oeps! an error occurred: ', { error, path });
+    },
+    modules: [
+      // final url: ./assets/i18n/en.json
+      { baseTranslateUrl },
+      // final url: ./assets/i18n/feature1/en.json
+      { baseTranslateUrl, moduleName: 'feature1' },
+      // final url: ./assets/i18n/feature2/en.json
+      { baseTranslateUrl, moduleName: 'feature2' },
+    ],
+  };
+  return new ModuleTranslateLoader(http, options);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: ModuleHttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
